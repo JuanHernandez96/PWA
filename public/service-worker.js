6943cee8b72c95c31ed2d1bfb077c9c757b77a6e
Juan Hernandez <juanh22@gmail.com>
2021-09-27 17:44:36 -0600

    added more code to service worker to try and fix heroku app

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,26 @@
 const APP_PREFIX = 'Budget-Tracker-';
-const VERSION = 'version_01';
+const VERSION = 'v1';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
 
+// caching more files 
 const FILES_TO_CACHE = [
-    "./js/idb.js",
-    "./js/index.js",
-    "./css/styles.css",
-    "./index.html",
+  "./index.html",
+  "./css/styles.css",
+  "./js/idb.js",
+  "./js/index.js",
+  "./manifest.json",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
 ]
 
+// first fetch response 
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
@@ -39,22 +51,59 @@ self.addEventListener('install', function (e) {
 
 
 
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-      caches.keys().then(function(keyList) {
-        let cacheKeeplist = keyList.filter(function(key) {
-          return key.indexOf(APP_PREFIX);
-        });
-        cacheKeeplist.push(CACHE_NAME);
 
-        return Promise.all(
-          keyList.map(function(key, i) {
-            if (cacheKeeplist.indexOf(key) === -1) {
-              console.log('deleting cache : ' + keyList[i]);
-              return caches.delete(keyList[i]);
+
+  // added another fetch request to see if this would help issue with service worker on heroku
+  self.addEventListener("fetch", function(e) {
+    if (e.request.url.includes("/api/")) {
+      e.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => { 
+          return fetch(e.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(e.request.url, response.clone());
             }
+
+            return response;
           })
-        );
-      })
+          .catch(err => {
+            return cache.match(e.request);
+          });
+      }).catch(err => console.log(err))
     );
-  });
+
+    return;
+  }
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (e.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
+
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
